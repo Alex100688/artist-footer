802d7fc6251a0811ce16f1ee3e89b7e69dbfdf2f
Alex100688 <alessiozingaro2@gmail.com>
2023-03-04 11:37:29 +0100

    inserito dei link sulle voci di AppFooterPrivacy

--- src/components/AppFooterPrivacy.vue
+++ src/components/AppFooterPrivacy.vue
@@ -6,9 +6,24 @@ export default {
 <template>
   <section>
     <ul>
-      <li><small>Privacy Policy</small></li>
-      <li><small>Social Media Policy</small></li>
-      <li><small>Copyright Notice</small></li>
+      <li>
+        <a
+          href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.prismic.io%2Flexdoit%2F650331da-fe13-4133-bf80-18635e63718e_Privacy-Policy.png%3Fauto%3Dcompress%2Cformat&imgrefurl=https%3A%2F%2Fwww.lexdo.it%2Fmodello%2Fprivacy-policy%2F&tbnid=ZXBkZeqdeK6SJM&vet=12ahUKEwiu14PXhcL9AhUWybsIHe1FBPwQMygCegUIARC_AQ..i&docid=S5u_k3R-L8CutM&w=1196&h=1696&q=esempi%20documento%20privacy&ved=2ahUKEwiu14PXhcL9AhUWybsIHe1FBPwQMygCegUIARC_AQ"
+          ><small>Privacy Policy</small></a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.sproutsocial.com%2Fuploads%2F2023%2F01%2FScreen-Shot-2022-12-22-at-12.30.38-PM.png&imgrefurl=https%3A%2F%2Fsproutsocial.com%2Finsights%2Fsocial-media-policy%2F&tbnid=9brLCRaf8dLY_M&vet=12ahUKEwiZptmkicL9AhWJmicCHURsCs4QMygCegUIARCyAQ..i&docid=7Lqm3ocfpJFo6M&w=1134&h=1266&q=social%20media%20pollicy%20example&ved=2ahUKEwiZptmkicL9AhWJmicCHURsCs4QMygCegUIARCyAQ"
+          ><small>Social Media Policy</small></a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.wordtemplatesonline.net%2Fwp-content%2Fuploads%2FCommon-Law-Copyright-6.png.webp&imgrefurl=https%3A%2F%2Fwww.wordtemplatesonline.net%2Fwriting-a-copyright-notice-with-samples%2F&tbnid=usmmtt9_740tjM&vet=12ahUKEwi4kufSiML9AhXGNOwKHRVFBJoQMygHegUIARDLAQ..i&docid=a-SZoRT1Zw2AvM&w=750&h=398&q=copyright%20notice%20example&ved=2ahUKEwi4kufSiML9AhXGNOwKHRVFBJoQMygHegUIARDLAQ"
+          ><small>Copyright Notice</small></a
+        >
+      </li>
     </ul>
   </section>
 </template>
@@ -16,5 +31,9 @@ export default {
 ul {
   list-style: none;
   color: #e5e5e5;
+}
+a {
+  text-decoration: none;
+  color: white;
 }
 </style>
